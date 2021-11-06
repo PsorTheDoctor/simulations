3c95bc732dfc6f691e708426027deada8f687725
PsorTheDoctor <psor2.0@gmail.com>
2021-11-06 15:54:05 +0100

    Initial commit

---
+++ examples/main.py
@@ -0,0 +1,19 @@
+import pybullet as p
+from time import sleep
+import pybullet_data
+
+client = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.81)
+planeId = p.loadURDF('plane.urdf')
+startPos = [0, 0, 1]
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+boxId = p.loadURDF('r2d2.urdf', startPos, startOrientation)
+
+for i in range(10000):
+    p.stepSimulation()
+    sleep(1./240.)
+
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos, cubeOrn)
+p.disconnect()
